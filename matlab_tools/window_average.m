ae5dd78dd5e269a70a3cae908a81e6d9574c6712
greyNearing <grey.nearing@gmail.com>
2016-12-27 11:40:03 -0500

    moved some things.

--- matlab_tools/window_average.m
+++ matlab_tools/window_average.m
@@ -22,33 +22,16 @@ for w = 1:W
  end
 end
 
+% remove most fo the redundancy
+if W > 2
+ keep = [1,round(W/2)];
+ Xw = Xw(:,keep);
+end
+
+%plot(Xw)
+%pause
+
+
 % make sure all data is filled
-%try
- assert(isempty(find(isnan(Xw),1,'first')))
-%catch
-% keyboard
-%end
-
-%% code below is for single chains.
-
-%% window starting indexes
-%sdex = 1:W:N-W;
-%
-%%initialize storage
-%Xw = zeros(length(sdex),D)-9999;
-%
-%% loop through dimensions of input data
-%for d = 1:D
-% for n = 1:length(sdex)
-%  Xw(n,d) = mean(X(sdex(n)+(0:W),d));
-% end
-%end
-%
-%
-%
-%
-%keyboard
-%
-%assert(isempty(find(Xw(:)<-9990)))
-
+assert(isempty(find(isnan(Xw),1,'first')))
 
